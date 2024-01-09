4f1596733392f5c5e4b2f03227330c7b3c505448
Tyre-228 <kiril228panko@gmail.com>
2024-01-09 22:37:52 +0000

    jdfs

--- backend/index.js
+++ backend/index.js
@@ -1,75 +1,73 @@
-// const express = require("express")
-// const path = require("path")
-// const {connectToDB, getDB} = require("./database")
-// const { ObjectId } = require("mongodb")
+const express = require("express")
+const path = require("path")
+const {connectToDB, getDB} = require("./database")
+const { ObjectId } = require("mongodb")
 
-// const app = express()
-// // app.use(express.json)
-// let db = ""
+const app = express()
+// app.use(express.json)
+let db = ""
 
-// connectToDB((err) => {
-//     if(!err) {
-//         app.listen(5000, () => {
-//             console.log("app listening on port 5000")
-//         })
-//         db = getDB()
-//     }
-// })
-// app.get("/", (req, res) => {
-//     res.send("Nothing")
-// })
-// app.get("/api", (req, res) => {
-//     res.send("API")
-// })
-// app.get("/api/users", (req, res) => {
-//     let data = []
-//     db.collection("users").find().sort().forEach(e => {
-//         data.push(e)
-//     })
-//     .then(() => {
-//         res.status(200).json(data)
-//     })
-//     .catch(() => {
-//         res.status(500).json({error: "Server error"})
-//     })
-// })
-// app.get("/api/users/:id", async (req, res) => {
-//     const id = req.params.id
-//     if(ObjectId.isValid(id)) {
-//         const result = await db.collection("users").findOne({"_id": new ObjectId(id)})
-//         res.status(200).json(result)
-//     }
-//     else {
-//         res.status(404).json({"status": "user not found"})
-//     }
-// })
-// app.get("/api/votings", (req, res) => {
-//     let data = []
-//     db.collection("votings").find().sort().forEach(e => {
-//         data.push(e)
-//     })
-//     .then(() => {
-//         res.status(200).json(data)
-//     })
-//     .catch(() => {
-//         res.status(500).json({error: "Server error"})
-//     })
-// })
-// app.get("/api/votings/:id", async (req, res) => {
-//     const id = req.params.id
-//     if(ObjectId.isValid(id)) {
-//         const result = await db.collection("votings").findOne({"_id": new ObjectId(id)})
-//         res.status(200).json(result)
-//     }
-//     else {
-//         res.status(404).json({"status": "voting not found"})
-//     }
-// })
-// app.all("*", (req, res) => {
-//     res.status("404").json({"status": "not found"})
-// })
-
-
+connectToDB((err) => {
+    if(!err) {
+        app.listen(5000, () => {
+            console.log("app listening on port 5000")
+        })
+        db = getDB()
+    }
+})
+app.get("/", (req, res) => {
+    res.send("Nothing")
+})
+app.get("/api", (req, res) => {
+    res.send("API")
+})
+app.get("/api/users", (req, res) => {
+    let data = []
+    db.collection("users").find().sort().forEach(e => {
+        data.push(e)
+    })
+    .then(() => {
+        res.status(200).json(data)
+    })
+    .catch(() => {
+        res.status(500).json({error: "Server error"})
+    })
+})
+app.get("/api/users/:id", async (req, res) => {
+    const id = req.params.id
+    if(ObjectId.isValid(id)) {
+        const result = await db.collection("users").findOne({"_id": new ObjectId(id)})
+        res.status(200).json(result)
+    }
+    else {
+        res.status(404).json({"status": "user not found"})
+    }
+})
+app.get("/api/votings", (req, res) => {
+    let data = []
+    db.collection("votings").find().sort().forEach(e => {
+        data.push(e)
+    })
+    .then(() => {
+        res.status(200).json(data)
+    })
+    .catch(() => {
+        res.status(500).json({error: "Server error"})
+    })
+})
+app.get("/api/votings/:id", async (req, res) => {
+    const id = req.params.id
+    if(ObjectId.isValid(id)) {
+        const result = await db.collection("votings").findOne({"_id": new ObjectId(id)})
+        res.status(200).json(result)
+    }
+    else {
+        res.status(404).json({"status": "voting not found"})
+    }
+})
+app.all("*", (req, res) => {
+    res.status("404").json({"status": "not found"})
+})
 
 
 
@@ -79,15 +77,6 @@
 
 
 
-
-
-
-exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Hello, World!" }),
-    };
-  };
 
 
 
