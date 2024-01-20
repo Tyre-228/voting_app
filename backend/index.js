6cbe39a3c76f11b0d57c1ce6bfcd31120d6d6cbb
Tyre-228 <kiril228panko@gmail.com>
2024-01-20 23:56:08 +0000

    fsafdsa

--- backend/index.js
+++ backend/index.js
@@ -19,9 +19,14 @@ connectToDB((err) => {
 })
 
 
-app.use(cors({
-    origin: "*"
-}))
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Replace '*' with the specific origin you want to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    // Continue to the next middleware
+    next();
+  });
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
